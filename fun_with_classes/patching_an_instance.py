a172afe4da9ee6db9c8e3613d8c4eee9f6b90db1
Sebastian Schönitz <sebastian.schoenitz@rwth-aachen.de>
2022-03-10 01:33:23 +0100

    changing a method for a class not instance

--- fun_with_classes/patching_an_instance.py
+++ fun_with_classes/patching_an_instance.py
@@ -4,6 +4,7 @@ import types
 class MyClass(object):
     def __init__(self) -> None:
         self.name = "MyClass"
+        self.foo = "Extra Info"
 
     def change_name_to(self, new_name) -> None:
         self.name = new_name
@@ -18,8 +19,16 @@ instance.change_name_to("foo")
 def name_is_allways_bar(self, new_name):
     self.name = "bar"
     print(self.name)
+    print(self.foo)
 
 
 instance.change_name_to = types.MethodType(name_is_allways_bar, instance)
 
 instance.change_name_to("Foo")
+
+print(name_is_allways_bar)
+
+MyClass.change_name_to = name_is_allways_bar
+
+instance2 = MyClass()
+instance2.change_name_to("foo")
